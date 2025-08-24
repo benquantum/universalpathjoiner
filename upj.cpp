ca2cb554f571fbd93aae476bf734154908fbe859
Ben <59486786+benquantum@users.noreply.github.com>
2025-08-25 00:22:51 +0200

    Code

--- upj.cpp
+++ upj.cpp
@@ -1,33 +1,42 @@
 #pragma region Universal Path Joiner
-//DO NOT call "pathJoinStr" (not recommended)
-#include <string>
-inline std::string pathJoinStr(const std::string& p) {
-    return p;
-}
-template<typename... Args>
-std::string pathJoinStr(const std::string& p1, const std::string& p2, Args... args) {
-    char sep = '/';
+//Example usage: pathJoin("assets","chewbacca","chewie.png")
+#include <cstring>
+#include <cstdlib>
 #ifdef _WIN32
-    sep = '\\';
+constexpr char SEP = '\\';
+#else
+constexpr char SEP = '/';
 #endif
-    std::string tmp = p1;
-    if (!tmp.empty() && tmp.back() != sep) {
-        tmp += sep;
-    }
-    std::string p2mod = p2;
-    if (!p2mod.empty() && p2mod.front() == sep) {
-        p2mod = p2mod.substr(1);
-    }
-    tmp += p2mod;
-    return pathJoinStr(tmp, args...);
+inline void appendPart(char* buffer, size_t& len, const char* part) {
+    if (!part || !*part) return;
+    if (len > 0 && buffer[len - 1] != SEP)
+        buffer[len++] = SEP, buffer[len] = '\0';
+    if (part[0] == SEP)
+        part++;
+    while (*part)
+        buffer[len++] = *part++;
+    buffer[len] = '\0';
 }
-//"pathJoin" wrapper function
+struct Path {
+    char* cstr;
+    Path(char* ptr) : cstr(ptr) {}
+    ~Path() { std::free(cstr); }
+
+    const char* get() const { return cstr; }
+    operator const char*() const { return cstr; }
+};
 template<typename... Args>
-const char* pathJoin(const std::string& p1, const std::string& p2, Args... args) {
-    static std::string tmp;
-    tmp = pathJoinStr(p1, p2, args...);
-    return tmp.c_str();
+Path pathJoin(const char* first, Args... args) {
+    size_t totalLen = std::strlen(first);
+    const char* parts[] = {args...};
+    for (const char* p : parts)
+        totalLen += std::strlen(p) + 1;
+    char* buffer = (char*)std::malloc(totalLen + 1);
+    if (!buffer) return Path(nullptr);
+    size_t len = 0;
+    appendPart(buffer, len, first);
+    for (const char* p : parts)
+        appendPart(buffer, len, p);
+    return Path(buffer);
 }
-//EXAMPLE CALL: pathJoin("assets", "3D", "Chewbacca", "chewie.png")
-//For root files, DO NOT use the function, just call "file.png"
 #pragma endregion
